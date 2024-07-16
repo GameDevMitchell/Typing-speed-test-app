e0a5bff55e76f6069c17a1f1ac8b90c500c22dda
GameDevMitchell <146736445+GameDevMitchell@users.noreply.github.com>
2024-07-16 22:34:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # setting up interfaces and common buttons
 # planning out the forms and design process
 # create requirements.txt and check  relevant dependencies
+# alongis
 
 import turtle
 ben = turtle.Turtle()
