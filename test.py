e3d27587b2f632c3e3ba597bd9dbee8f9d05e3fc
GameDevMitchell <146736445+GameDevMitchell@users.noreply.github.com>
2024-08-17 00:02:46 +0300

    update test.py

--- test.py
+++ test.py
@@ -19,4 +19,4 @@ def create_phone_number(n):
 
 create_phone_number(([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
 print("Hello")
-print("Success at the moment, I guess.....")
+print("Success at the moment, I guess...")
